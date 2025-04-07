b043f4e76d835243f4cd8342ef1152296bd6cb66
Marcella <marcellaacoelho@gmail.com>
2025-04-07 11:54:06 -0300

    Ajustes finais

---
+++ src/main/java/br/com/gerenciador/api/dto/EstatisticasResponseDTO.java
@@ -0,0 +1,11 @@
+package br.com.gerenciador.api.dto;
+
+public record EstatisticasResponseDTO(
+
+        Long totalFornecedores,
+        Long totalProdutos,
+        Long totalClientes
+
+) {
+
+}
